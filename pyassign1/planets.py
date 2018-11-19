7eb6af9c7db5493b1d8bff4308fe2b1ffe17b781
WTFe <2294374906@qq.com>
2018-11-19 21:20:50 +0800

    Update planets.py

--- pyassign1/planets.py
+++ pyassign1/planets.py
@@ -1,10 +1,22 @@
+"""planets.py: It could imitate the motions of
+               the inner 6 planets of the solar
+               system.(circle orbits, radius 
+               based on real sizes)
+
+__author__ = "Wangtie"
+__pkuid__  = "1800011811"
+__email__  = "1800011811@pku.edu.cn"
+"""
+
 import turtle
 import math
-r_earth=30
+r_earth=30 #alter this to put the orbits in screen#
 t0=turtle.Turtle()
 t0.color('yellow')
 t0.shape('circle')
 t0.stamp()
+# the sun #
+
 t1=turtle.Turtle()
 t1.color('blue')
 t1.shape('circle')
@@ -12,6 +24,8 @@ t1.shapesize(0.19,0.19)
 t1.penup()
 t1.goto(0,-0.38*r_earth)
 t1.pendown()
+# mercury #
+
 t2=turtle.Turtle()
 t2.color('orange')
 t2.shape('circle')
@@ -19,6 +33,8 @@ t2.shapesize(0.3,0.3)
 t2.penup()
 t2.goto(0,-0.52*r_earth)
 t2.pendown()
+# venus #
+
 t3=turtle.Turtle()
 t3.color('green')
 t3.shape('circle')
@@ -26,6 +42,8 @@ t3.shapesize(0.5,0.5)
 t3.penup()
 t3.goto(0,-1*r_earth)
 t3.pendown()
+# earth #
+
 t4=turtle.Turtle()
 t4.color('red')
 t4.shape('circle')
@@ -33,6 +51,8 @@ t4.shapesize(0.26,0.26)
 t4.penup()
 t4.goto(0,-1.52*r_earth)
 t4.pendown()
+# mars #
+
 t5=turtle.Turtle()
 t5.color('brown')
 t5.shape('circle')
@@ -40,6 +60,8 @@ t5.shapesize(5.5,5.5)
 t5.penup()
 t5.goto(0,-5.2*r_earth)
 t5.pendown()
+# jupiter #
+
 t6=turtle.Turtle()
 t6.color('gray')
 t6.shape('circle')
@@ -47,9 +69,10 @@ t6.shapesize(4.72,4.72)
 t6.penup()
 t6.goto(0,-9.54*r_earth)
 t6.pendown()
+# saturn #
 
 for m in range(200):
-    b=15 #speed modifier#
+    b=15 #speed modifier of circling#
     t1.speed(0)
     t2.speed(0)
     t3.speed(0)
