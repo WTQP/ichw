624b80e25ef76443867f4e4542ad5c3e9c6ad487
WTFe <2294374906@qq.com>
2018-12-17 23:55:11 +0800

    Update tiles.py

--- pyassign3/tiles.py
+++ pyassign3/tiles.py
@@ -1,7 +1,7 @@
 """Tiles.py: 设计目标：对任意给定长方形墙面和长方形砖块，输出合理的用砖块铺满这个墙壁
              的方法，并用 Turtle 模块实现可视化。
 
-             但目前尚未完成，目前程序仅能尝试出部分简单的解法，而且无法有效输出。
+             但目前尚未完成，目前程序仅能尝试出特定的一种解法，而且无法有效输出。
 
 __author__ = "Wangtie"
 __pkuid__  = "1800011811"
@@ -14,45 +14,42 @@ def tstheng(next, qiang):
     global a, c, d
     for k in range(c):
         if qiang[next % a + k] != 0:
-            print('falseh')
+            # 若任意同行小块被占据，则报错
             return False
         else:
             for m in range(d):
                 if qiang[next % a + k + m * a] != 0:
-                    # 若有任一块非空，就会冲突。
-                    print('falseh')
+                    # 若下面的任意小块被占据，则报错
                     return False
                 else:
                     pass
-    return True  # 每块儿都没问题
+    return True  # 每一块都没问题，可以横着铺
 
 
 def tstzong(next, qiang):
-    '''检查下一块砖能不能横着铺'''
+    '''检查下一块砖能不能竖着铺'''
     global a, c, d
     for k in range(d):
         if qiang[next % a + k] != 0:
-            print('falsez')
             return False
         else:
             for m in range(c):
                 if qiang[next % a + k + m * a] != 0:
-                    # 若有任一块非空，就会冲突。
-                    print('falsez')
                     return False
                 else:
                     pass
-    return True  # 每一块都没有问题
+    return True  # 每一块都没问题，可以竖着铺
+
 
 def pu(ans, qiang):
-    global a, b, c, d
+    global a, b, c, d, qiangf
     if qiang == qiangf:
-        return ans
         print('done')
+        print(ans)
+        return ans
     else:
         next = qiang.index(0)
-        if tstheng(next， qiang):
-            ans = []
+        if tstheng(next, qiang):
             ans1 = [(next % a + k + m * a) for k in range(c)
                     for m in range(d)]
             for i in ans1:
@@ -64,8 +61,8 @@ def pu(ans, qiang):
             # 为了尝试竖着铺，因此需要把铺好的瓷砖先拆下来
             lastans = ans.pop()  # 从答案总集中去除上一块
             for i in lastans:
-                qiang[i] = 0  # 从墙上取下上一块
-        if tstzong(next， qiang):
+                qiang[i] = 0  # 从墙上去除上一块
+        if tstzong(next, qiang):
             ans1 = [(next % a + k + m * a) for k in range(d)
                     for m in range(c)]
             for i in ans1:
@@ -77,23 +74,21 @@ def pu(ans, qiang):
             # 为了尝试竖着铺，因此需要把铺好的瓷砖先拆下来
             lastans = ans.pop()  # 从答案总集中去除上一块
             for i in lastans:
-                qiang[i] = 0  # 从墙上取下上一块
-    # 运行到这里说明两种方法都尝试过了，回溯到上一个瓷砖尝试铺法
+                qiang[i] = 0  # 从墙上清除上一块
     return
 
 
-if '__name__' == '__main__':
-    a = int(input('墙的长度是多少？'))
-    b = int(input('墙的高度是多少？'))
-    c = int(input('砖的长度是多少？'))
-    d = int(input('砖的宽度是多少？'))
-    if (a * b) % (c * d) != 0 or c > a or d > b \
-       or a == 0 or b == 0 or c == 0 or d == 0:
-        print('这组数据铺不了哟~')
-    else:
-        qiang = []
-        qiangf = []
-        for j in range(a * b):
-            qiang.append(0)  # 初始化墙壁
-            qiangf.append(1)
-        pu([], qiang)
+a = 3  # 这是目前唯一能给一组解的一组数据
+b = 2
+c = 2
+d = 1
+if (a * b) % (c * d) != 0 or c > a or d > b \
+   or a == 0 or b == 0 or c == 0 or d == 0:
+    print('这组数据铺不了哟~')
+else:
+    qiang = []
+    qiangf = []
+    for j in range(a * b):
+        qiang.append(0)  # 初始化墙壁
+        qiangf.append(1)  # 设置对照的贴满墙壁
+    pu([], qiang)
